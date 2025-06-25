fb83bf2598a377a5b5f34af3981871b63f43ac0b
Aedo F <f.aedo@duocuc.cl>
2025-06-24 20:54:24 -0400

    Creacion de pages

---
+++ BuenPrecio/src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'add-product',
+    loadChildren: () => import('./add-product/add-product.module').then( m => m.AddProductPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'my-offers',
+    loadChildren: () => import('./my-offers/my-offers.module').then( m => m.MyOffersPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'supported-pages',
+    loadChildren: () => import('./supported-pages/supported-pages.module').then( m => m.SupportedPagesPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ BuenPrecio/src/app/supported-pages/supported-pages.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-supported-pages',
+  templateUrl: './supported-pages.page.html',
+  styleUrls: ['./supported-pages.page.scss'],
+})
+export class SupportedPagesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
